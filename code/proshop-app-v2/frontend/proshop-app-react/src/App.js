89610ba96aeb4116b836eb4e745bcdb335cb1817
TrainingMyHobby <javakishore@gmail.com>
2021-02-02 15:15:47 -0500

    Added npm install typescript and  npm install react-bootstrap and npm install react-router-dom

--- code/proshop-app-v2/frontend/proshop-app-react/src/App.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Header from './modules/shared/layout/Header';
+import Footer  from './modules/shared/layout/Footer';
 
 const App = () => {
   return (
     <div className="App">
+      <Header></Header>
+
+      <Footer></Footer>
     </div>
   );
 }
