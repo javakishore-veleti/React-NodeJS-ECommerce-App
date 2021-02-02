d94d9c2bb597e0a24cce224857b08431b167bd3a
TrainingMyHobby <javakishore@gmail.com>
2021-02-02 16:29:36 -0500

    Added <Link react-router-dom for NavBar

--- code/proshop-app-v2/frontend/proshop-app-react/src/App.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Header from './modules/shared/layout/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
     <>
     <BrowserRouter>
       <Header></Header>
-      <div className="content">
-        <h1>Welcome!</h1>
-      </div>
+      <main>
+        <Container>
+          <div className="content">
+            <h1>Welcome!</h1>
+          </div>
+      </Container>
+      </main>
     </BrowserRouter>
   </>
   );
