1b1cf9a72337ddfdf2792f0e9933cddea27748c3
TrainingMyHobby <javakishore@gmail.com>
2021-02-02 16:37:45 -0500

    Added jquery popper.js font-awesome

--- code/proshop-app-v2/frontend/proshop-app-react/src/modules/shared/layout/NavBar.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/modules/shared/layout/NavBar.js
@@ -1,5 +1,5 @@
 
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const AppNavBar = () => {
@@ -7,19 +7,24 @@ const AppNavBar = () => {
     return (
 
         <>
-            <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href="#home">Pipelines</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <NavItem><Link className="nav-link" to="/">Home</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/dashboard">Dashboard</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/projects">Projects</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/administration">Administration</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/signup">Sign Up</Link></NavItem>
-                    <NavItem><Link className="nav-link" to="/signin">Sign In</Link></NavItem>
-                </Nav>
-            </Navbar>
+            <header>
+                <Navbar bg="primary" variant="dark" collapseOnSelect>
+                    <Container>
+                        <Navbar.Brand href="#home">Pipelines</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <NavItem><Link className="nav-link" to="/">Home</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/dashboard">Dashboard</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/projects">Projects</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/administration">Administration</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/signup">Sign Up</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/signin">Sign In</Link></NavItem>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                
+            </header>
         </>
 
     );
