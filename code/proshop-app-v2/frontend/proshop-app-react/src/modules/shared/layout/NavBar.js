af3019c1b6d0d139049bba3d64322d157da95a0a
TrainingMyHobby <javakishore@gmail.com>
2021-02-02 17:47:58 -0500

    Added fontawesome icons to the Menu items

--- code/proshop-app-v2/frontend/proshop-app-react/src/modules/shared/layout/NavBar.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/modules/shared/layout/NavBar.js
@@ -1,8 +1,12 @@
 
 import { Navbar, Nav, NavItem, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignInAlt, faRegistered, faHome, faProjectDiagram, faToolbox, faTable, faTachometerAlt } from '@fortawesome/free-solid-svg-icons'
 
 const AppNavBar = () => {
+
+    //const faCoffeeEleem = 
 
     return (
 
@@ -12,14 +16,13 @@ const AppNavBar = () => {
                     <Container>
                         <Navbar.Brand href="#home">Pipelines</Navbar.Brand>
                         <Nav className="mr-auto">
-                            <NavItem><Link className="nav-link" to="/">Home</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/dashboard">Dashboard</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/projects">Projects</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/administration">Administration</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/dataconnections">Data Connections</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/signup">Sign Up</Link></NavItem>
-                            <NavItem><Link className="nav-link" to="/signin">Sign In</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/dashboard"><FontAwesomeIcon icon={faTachometerAlt} /> Dashboard</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/projects"><FontAwesomeIcon icon={faProjectDiagram} /> Projects</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/administration"><FontAwesomeIcon icon={faToolbox} /> Administration</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/dataconnections"><FontAwesomeIcon icon={faTable} /> Data Connections</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/signup"><FontAwesomeIcon icon={faRegistered} /> Sign Up</Link></NavItem>
+                            <NavItem><Link className="nav-link" to="/signin"><FontAwesomeIcon icon={faSignInAlt} /> Sign In</Link></NavItem>
                         </Nav>
                     </Container>
                 </Navbar>
