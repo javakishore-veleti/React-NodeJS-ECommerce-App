29bb6123bd48e215793c85c1f6d0d1321fb4a5af
TrainingMyHobby <javakishore@gmail.com>
2021-02-03 04:49:12 -0500

    Added formik sigin form code

--- code/proshop-app-v2/frontend/proshop-app-react/src/modules/aas/components/SignIn.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/modules/aas/components/SignIn.js
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import SigInSchmea from '../validatioins/SignInValidation';
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
 
 const SignIn = () => {
 
@@ -10,48 +9,53 @@ const SignIn = () => {
         password: ""
       };
 
-
-    const validaeUserSignIn = async (event) => {
-        event.preventDefault();
-        const signInFormData =  {
-            email: event.target[0].value,
-            password: event.target[1].value
-        };
-
-        const isValid = await SigInSchmea.isValid(signInFormData);
+    const handleSubmit = (values) => {
+        console.log("handleSubmit ", values);
     }
-
 
     return (
         <>
-            <Container>
-                <div>
-                    <h3>Sign In</h3>
-                    <Form onSubmit={validaeUserSignIn} noValidate>
-                        <Row>
-                            <Col>
-                                <div className="form-group">
-                                    <label>Email</label>
-                                    <input type="email" className="form-control" placehlder="Email" />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input type="password" className="form-control" placehlder="Password" />
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </div>
-            </Container>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={SigInSchmea}
+            onSubmit={handleSubmit}
+        >
+            {(formik) => {
+                const { errors, touched, isValid } = formik;
+                return (
+                    <Container>
+                        <div>
+                            <h3>Sign In</h3>
+                            <Form noValidate >
+                                <Row>
+                                    <Col>
+                                        <div className="form-group">
+                                            <label htmlFor="email">Email</label>
+                                            <Field type="email" name="email" id="email" placehlder="Email" className={errors.email && touched.email ? "form-control has-form-field-error " : "form-control"} />
+                                            <ErrorMessage name="email" component="span" className="error has-error" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <div className="form-group">
+                                            <label>Password</label>
+                                            <Field type="password" name="password" id="password" placehlder="Email" className={errors.password && touched.password ? "form-control has-form-field-error " : "form-control"} />
+                                            <ErrorMessage name="password" component="span" className="error" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col>
+                                        <button type="submit" disabled={!isValid} className="btn btn-primary btn-block">Submit</button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
+                    </Container>
+                );
+            }}
+            </Formik>
         </>
     )
 }
--- code/proshop-app-v2/frontend/proshop-app-react/src/modules/aas/validatioins/SignInValidation.js
+++ code/proshop-app-v2/frontend/proshop-app-react/src/modules/aas/validatioins/SignInValidation.js
@@ -2,7 +2,7 @@ import * as yup from "yup";
 
 const SigInSchmea = yup.object().shape( {
     email: yup.string().min(5, "Too Short!").max(50, "Too Long!").required("Email is required").email().required(),
-    password: yup.string().min(4).max(4).required()
+    password: yup.string().min(4).max(20).required()
 });
 
 export default SigInSchmea;
